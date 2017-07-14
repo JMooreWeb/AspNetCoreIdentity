20b08a93492d5a06007f514d4d402d14cfaf527a
James Moore <james.moore@bcpao.us>
2017-07-14 06:12:29 -0400

    Add project files.

---
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
